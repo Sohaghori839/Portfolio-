ae30553bd883e4fd385eddf27826e88b4e53627c
Sohaghori839 <sohaghori839@gmail.com>
2023-04-02 09:36:31 +0500

    portfolio

---
+++ innovative/js/script.js
@@ -0,0 +1,197 @@
+$(window).on("load", function () {
+
+    "use strict";
+
+    /* ===================================
+            Loading Timeout
+     ====================================== */
+
+    $('.side-menu.hidden').removeClass('hidden');
+
+    setTimeout(function () {
+        $(".loader").fadeOut("slow");
+    }, 1000);
+
+
+});
+
+jQuery(function ($) {
+
+
+    "use strict";
+
+
+    /* ===================================
+         Scroll
+    ====================================== */
+
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 220) { // Set position from top to add class
+            $('header').addClass('header-appear');
+        }
+        else {
+            $('header').removeClass('header-appear');
+        }
+    });
+
+    //scroll to appear
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 500)
+            $('.scroll-top-arrow').fadeIn('slow');
+        else
+            $('.scroll-top-arrow').fadeOut('slow');
+    });
+
+    //Click event to scroll to top
+    $(document).on('click', '.scroll-top-arrow', function () {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
+
+    $(".scroll").on("click", function (event) {
+        event.preventDefault();
+        $("html,body").animate({
+            scrollTop: $(this.hash).offset().top - 60}, 1200);
+    });
+
+
+    /* =====================================
+           Parallax
+    ====================================== */
+
+    if ($(window).width() > 992) {
+        $(".parallax").parallaxie({
+            speed: 0.55,
+            offset: 0,
+        });
+    }
+
+
+    /* ===================================
+         Side Menu
+    ====================================== */
+    if ($("#sidemenu_toggle").length) {
+        $("#sidemenu_toggle").on("click", function () {
+            $(".pushwrap").toggleClass("active");
+            $(".side-menu").addClass("side-menu-active"), $("#close_side_menu").fadeIn(700)
+        }), $("#close_side_menu").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $(this).fadeOut(200), $(".pushwrap").removeClass("active")
+        }), $(".side-nav .navbar-nav .nav-link").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        }), $("#btn_sideNavClose").on("click", function () {
+            $(".side-menu").removeClass("side-menu-active"), $("#close_side_menu").fadeOut(200), $(".pushwrap").removeClass("active")
+        });
+    }
+
+
+});
+/* card */
+(function($) {
+    $.fn.countTo = function(options) {
+      options = options || {};
+  
+      return $(this).each(function() {
+        // set options for current element
+        var settings = $.extend(
+          {},
+          $.fn.countTo.defaults,
+          {
+            from: $(this).data("from"),
+            to: $(this).data("to"),
+            speed: $(this).data("speed"),
+            refreshInterval: $(this).data("refresh-interval"),
+            decimals: $(this).data("decimals")
+          },
+          options
+        );
+  
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(settings.speed / settings.refreshInterval),
+          increment = (settings.to - settings.from) / loops;
+  
+        // references & variables that will change with each update
+        var self = this,
+          $self = $(this),
+          loopCount = 0,
+          value = settings.from,
+          data = $self.data("countTo") || {};
+  
+        $self.data("countTo", data);
+  
+        // if an existing interval can be found, clear it first
+        if (data.interval) {
+          clearInterval(data.interval);
+        }
+        data.interval = setInterval(updateTimer, settings.refreshInterval);
+  
+        // initialize the element with the starting value
+        render(value);
+  
+        function updateTimer() {
+          value += increment;
+          loopCount++;
+  
+          render(value);
+  
+          if (typeof settings.onUpdate == "function") {
+            settings.onUpdate.call(self, value);
+          }
+  
+          if (loopCount >= loops) {
+            // remove the interval
+            $self.removeData("countTo");
+            clearInterval(data.interval);
+            value = settings.to;
+  
+            if (typeof settings.onComplete == "function") {
+              settings.onComplete.call(self, value);
+            }
+          }
+        }
+  
+        function render(value) {
+          var formattedValue = settings.formatter.call(self, value, settings);
+          $self.html(formattedValue);
+        }
+      });
+    };
+  
+    $.fn.countTo.defaults = {
+      from: 0, // the number the element should start at
+      to: 0, // the number the element should end at
+      speed: 1000, // how long it should take to count between the target numbers
+      refreshInterval: 100, // how often the element should be updated
+      decimals: 0, // the number of decimal places to show
+      formatter: formatter, // handler for formatting the value before rendering
+      onUpdate: null, // callback method for every time the element is updated
+      onComplete: null // callback method for when the element finishes updating
+    };
+  
+    function formatter(value, settings) {
+      return value.toFixed(settings.decimals);
+    }
+  })(jQuery);
+  
+  jQuery(function($) {
+    // custom formatting example
+    $(".count-number").data("countToOptions", {
+      formatter: function(value, options) {
+        return value
+          .toFixed(options.decimals)
+          .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+      }
+    });
+  
+    // start all the timers
+    $(".timer").each(count);
+  
+    function count(options) {
+      var $this = $(this);
+      options = $.extend({}, options || {}, $this.data("countToOptions") || {});
+      $this.countTo(options);
+    }
+  });
+  // projects
+  const projectName = 'portfolio';
+  // testimonial
